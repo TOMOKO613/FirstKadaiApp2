eb9338722a06e480654cc990b6cb019f59031f4e
tomoko.nobata <mokotan2142@gmail.com>
2021-12-19 00:54:57 +0900

    Initial Commit

---
+++ SecondKadaiApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp2
+//
+//  Created by Owner on 2021/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
